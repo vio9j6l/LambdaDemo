68614cdb1af9134e1725adc8061f04b4c78a1259
JJ <jiajia.liu951ss@gmail.com>
2020-11-19 08:49:21 -0500

    unsorted list

--- src/Main.java
+++ src/Main.java
@@ -43,6 +43,8 @@ public class Main {
         System.out.println("\nLambda 4: ");
         people.forEach(System.out::println);
 
+        System.out.println("----------------------------------------------------");
+        System.out.println("\nUnsorted:");
         for (Person person : people) {
             System.out.println(person);
         }
